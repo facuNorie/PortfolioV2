377430787ed425cd42a342b70841560b409b004e
Facu Noriega <noriegafacundo08@gmail.com>
2021-11-20 00:07:50 -0300

    Adding social medias & toast

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import {
   Box,
-  Text,
   Flex,
   Image,
   FormControl,
   FormLabel,
   Input,
   Button,
+  Textarea,
+  Text,
+  useToast,
 } from '@chakra-ui/react';
 import emailjs from 'emailjs-com';
 import Title from '../Title';
 import { SERVICE, TEMPLATE, USER } from '../../helpers/privateKeys';
+import email from '../../assets/email.png';
 
 export default function Contact() {
+  const toast = useToast();
+
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs
@@ -25,7 +30,15 @@ export default function Contact() {
       )
       .then(
         (result) => {
-          console.log(result.text);
+          result.text === 'OK' &&
+            toast({
+              title: 'Mensaje enviado.',
+              position: 'top-right',
+              description: 'Tu mensaje ha sido enviado exitosamente',
+              status: 'success',
+              duration: 2000,
+              isClosable: true,
+            });
         },
         (error) => {
           console.log(error.text);
@@ -33,30 +46,74 @@ export default function Contact() {
       );
     e.target.reset();
   };
+
   return (
-    <article id='contact' style={{ marginTop: '200px', height: '100vh' }}>
+    <article
+      id='contact'
+      style={{ paddingTop: '200px', paddingBottom: '150px' }}
+    >
       <Box textAlign='center'>
-        <Title title='Contact' colorA='#d12c2c' colorB='#414f9e' />
+        <Title title='Contacto' colorA='#d12c2c' colorB='#414f9e' />
       </Box>
-      <Box>
-        <form onSubmit={sendEmail}>
-          <FormControl>
-            <FormLabel>Name</FormLabel>
-            <Input type='text' name='name' />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input type='email' name='email' />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Message</FormLabel>
-            <Input type='text' name='message' />
-          </FormControl>
-          <Button type='submit' bgColor='whatsapp.500'>
-            Submit
-          </Button>
-        </form>
-      </Box>
+      <Flex
+        justify='space-evenly'
+        align='center'
+        mt={15}
+        direction={['column', 'column', 'row', 'row']}
+      >
+        <Box w={{ base: '80%', md: '35%' }}>
+          <form onSubmit={sendEmail}>
+            <FormControl mb={3}>
+              <FormLabel>Nombre</FormLabel>
+              <Input type='text' name='name' autoComplete='off' required />
+            </FormControl>
+            <FormControl mb={3}>
+              <FormLabel>Email</FormLabel>
+              <Input type='email' name='email' autoComplete='off' required />
+            </FormControl>
+            <FormControl mb={3}>
+              <FormLabel>Mensaje</FormLabel>
+              <Textarea type='text' name='message' resize='none' required />
+            </FormControl>
+            <div style={{ textAlign: 'center' }}>
+              <Button
+                type='submit'
+                bgGradient='linear(to-r,#d12c2c,#414f9e)'
+                color='white'
+                boxShadow='2xl'
+                _hover={{
+                  backgroundColor: 'none',
+                }}
+              >
+                Contactar
+              </Button>
+            </div>
+          </form>
+        </Box>
+        <Box display='flex' flexDirection='column' alignItems='center'>
+          <Image
+            src={email}
+            alt='Email contact picture'
+            boxSize={{ base: '200px', md: '350px' }}
+          />
+          <Box>
+            <Flex mb={2}>
+              <i
+                className='bx bxl-linkedin'
+                style={{ fontSize: '1.5rem', marginRight: '.5rem' }}
+              ></i>
+              <Text fontWeight='bold'>facuNorie</Text>
+            </Flex>
+            <Flex align='center'>
+              <i
+                className='bx bx-mail-send'
+                style={{ fontSize: '1.5rem', marginRight: '.5rem' }}
+              ></i>
+              <Text fontWeight='bold'>noriegafacundo08@gmail.com</Text>
+            </Flex>
+          </Box>
+        </Box>
+      </Flex>
     </article>
   );
 }
