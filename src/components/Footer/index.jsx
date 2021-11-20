c7cc3f1b5e851edda9d6a99e7f54439bba72e279
Facu Noriega <noriegafacundo08@gmail.com>
2021-11-20 00:04:45 -0300

    Using chakra color dark mode

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Box, Text, useColorMode } from '@chakra-ui/react';
 export default function Footer() {
   const { colorMode } = useColorMode();
-  let bgColor = colorMode === 'light' ? 'black' : 'whiteAlpha.800';
+  let bgColor = colorMode === 'light' ? 'gray.800' : 'white';
   let color = colorMode === 'light' ? 'white' : 'black';
   return (
-    <Box textAlign='center' bgColor={bgColor} py={2}>
+    <Box textAlign='center' bgColor={bgColor} py={1}>
       <Text color={color}>Hecho con 💙 por Facu Noriega</Text>
     </Box>
   );
